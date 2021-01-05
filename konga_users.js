fd5971f22bc06f36d128b77584aff41a3246c5c9
Vahid <vahidzafari@outlook.com>
2021-01-05 22:05:11 +0330

    add konga static users

---
+++ konga_users.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    username: "admin",
+    email: "john@some.test",
+    firstName: "John",
+    lastName: "Doe",
+    node_id: "http://kong:8001",
+    admin: true,
+    active: true,
+    password: "adminadminadmin",
+  },
+];
